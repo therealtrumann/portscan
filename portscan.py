daad23917ebb7b42ccc193d16cb937b11d510b98
therealtrumann <pauloeduardopereiraa@gmail.com>
2023-11-21 10:41:47 -0300

    portscan for pentest

---
+++ portscan.py
@@ -0,0 +1,10 @@
+import socket #biblioteca
+
+ports = [21,22,80,443,445,3306,25]
+
+for port in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(0.1)
+	code = client.connect_ex(("DOMINIO.COM", port))
+	if code == 0:
+		print(port, "OPEN")
